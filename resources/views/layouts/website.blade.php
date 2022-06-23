36fbad3f57f569f901bc564794522f7d3a3321ce
Md Rubel Rana <rubel.rana@northsouth.edu>
2022-06-23 16:11:23 +0600

    homepage added

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -7,17 +7,17 @@
 
     <link href="../../css.css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/fonts/icomoon/style.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('website') }}/css/style.css">
   </head>
   <body>
   
@@ -70,14 +70,14 @@
       <div class="container">
         <div class="row align-items-stretch retro-layout-2">
           <div class="col-md-4">
-            <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('images/img_1.jpg');">
+            <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ asset('website') }}/images/img_1.jpg');">
               
               <div class="text">
                 <h2>The AI magically removes moving objects from videos.</h2>
                 <span class="date">July 19, 2019</span>
               </div>
             </a>
-            <a href="single.html" class="h-entry v-height gradient" style="background-image: url('images/img_2.jpg');">
+            <a href="single.html" class="h-entry v-height gradient" style="background-image: url('{{ asset('website') }}/images/img_2.jpg');">
               
               <div class="text">
                 <h2>The AI magically removes moving objects from videos.</h2>
@@ -86,7 +86,7 @@
             </a>
           </div>
           <div class="col-md-4">
-            <a href="single.html" class="h-entry img-5 h-100 gradient" style="background-image: url('images/img_v_1.jpg');">
+            <a href="single.html" class="h-entry img-5 h-100 gradient" style="background-image: url('{{ asset('website') }}/images/img_v_1.jpg');">
               
               <div class="text">
                 <div class="post-categories mb-3">
@@ -99,14 +99,14 @@
             </a>
           </div>
           <div class="col-md-4">
-            <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('images/img_3.jpg');">
+            <a href="single.html" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ asset('website') }}/images/img_3.jpg');">
               
               <div class="text">
                 <h2>The 20 Biggest Fintech Companies In America 2019</h2>
                 <span class="date">July 19, 2019</span>
               </div>
             </a>
-            <a href="single.html" class="h-entry v-height gradient" style="background-image: url('images/img_4.jpg');">
+            <a href="single.html" class="h-entry v-height gradient" style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
               
               <div class="text">
                 <h2>The 20 Biggest Fintech Companies In America 2019</h2>
@@ -128,13 +128,13 @@
         <div class="row">
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-secondary mb-3">Politics</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -146,13 +146,13 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-success mb-3">Nature</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -164,13 +164,13 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_3.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_3.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-warning mb-3">Travel</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -184,13 +184,13 @@
 
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-secondary mb-3">Politics</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -202,13 +202,13 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-success mb-3">Nature</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -220,13 +220,13 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_4.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_4.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-danger mb-3">Sports</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -240,13 +240,13 @@
 
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_1.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-success mb-3">Nature</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -258,14 +258,14 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_2.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-danger mb-3">Sports</span>
               <span class="post-category text-white bg-secondary mb-3">Tech</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -276,14 +276,14 @@
           </div>
           <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="images/img_4.jpg" alt="Image" class="img-fluid rounded"></a>
+              <a href="single.html"><img src="{{ asset('website') }}/images/img_4.jpg" alt="Image" class="img-fluid rounded"></a>
               <div class="excerpt">
               <span class="post-category text-white bg-danger mb-3">Sports</span>
               <span class="post-category text-white bg-warning mb-3">Lifestyle</span>
 
               <h2><a href="single.html">The AI magically removes moving objects from videos.</a></h2>
               <div class="post-meta align-items-center text-left clearfix">
-                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ asset('website') }}/images/person_1.jpg" alt="Image" class="img-fluid"></figure>
                 <span class="d-inline-block mt-1">By <a href="#">Carrol Atkinson</a></span>
                 <span>&nbsp;-&nbsp; July 19, 2019</span>
               </div>
@@ -315,7 +315,7 @@
         <div class="row align-items-stretch retro-layout">
           
           <div class="col-md-5 order-md-2">
-            <a href="single.html" class="hentry img-1 h-100 gradient" style="background-image: url('images/img_4.jpg');">
+            <a href="single.html" class="hentry img-1 h-100 gradient" style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
               <span class="post-category text-white bg-danger">Travel</span>
               <div class="text">
                 <h2>The 20 Biggest Fintech Companies In America 2019</h2>
@@ -326,7 +326,7 @@
 
           <div class="col-md-7">
             
-            <a href="single.html" class="hentry img-2 v-height mb30 gradient" style="background-image: url('images/img_1.jpg');">
+            <a href="single.html" class="hentry img-2 v-height mb30 gradient" style="background-image: url('{{ asset('website') }}/images/img_1.jpg');">
               <span class="post-category text-white bg-success">Nature</span>
               <div class="text text-sm">
                 <h2>The 20 Biggest Fintech Companies In America 2019</h2>
@@ -335,14 +335,14 @@
             </a>
             
             <div class="two-col d-block d-md-flex">
-              <a href="single.html" class="hentry v-height img-2 gradient" style="background-image: url('images/img_2.jpg');">
+              <a href="single.html" class="hentry v-height img-2 gradient" style="background-image: url('{{ asset('website') }}/images/img_2.jpg');">
                 <span class="post-category text-white bg-primary">Sports</span>
                 <div class="text text-sm">
                   <h2>The 20 Biggest Fintech Companies In America 2019</h2>
                   <span>February 12, 2019</span>
                 </div>
               </a>
-              <a href="single.html" class="hentry v-height img-2 ml-auto gradient" style="background-image: url('images/img_3.jpg');">
+              <a href="single.html" class="hentry v-height img-2 ml-auto gradient" style="background-image: url('{{ asset('website') }}/images/img_3.jpg');">
                 <span class="post-category text-white bg-warning">Lifestyle</span>
                 <div class="text text-sm">
                   <h2>The 20 Biggest Fintech Companies In America 2019</h2>
@@ -427,19 +427,19 @@
     
   </div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/aos.js"></script>
+  <script src="{{ asset('website') }}/js/jquery-3.3.1.min.js"></script>
+  <script src="{{ asset('website') }}/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="{{ asset('website') }}/js/jquery-ui.js"></script>
+  <script src="{{ asset('website') }}/js/popper.min.js"></script>
+  <script src="{{ asset('website') }}/js/bootstrap.min.js"></script>
+  <script src="{{ asset('website') }}/js/owl.carousel.min.js"></script>
+  <script src="{{ asset('website') }}/js/jquery.stellar.min.js"></script>
+  <script src="{{ asset('website') }}/js/jquery.countdown.min.js"></script>
+  <script src="{{ asset('website') }}/js/jquery.magnific-popup.min.js"></script>
+  <script src="{{ asset('website') }}/js/bootstrap-datepicker.min.js"></script>
+  <script src="{{ asset('website') }}/js/aos.js"></script>
 
-  <script src="js/main.js"></script>
+  <script src="{{ asset('website') }}/js/main.js"></script>
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async="" src="../../gtag/js.js?id=UA-23581568-13"></script>
