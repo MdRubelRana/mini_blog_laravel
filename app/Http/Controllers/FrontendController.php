c1e9c8aa3a3399c69f07ebb03768a9e526ef45c1
Md Rubel Rana <rubel.rana@northsouth.edu>
2022-06-27 13:17:08 +0600

    random post fetched

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,18 +9,27 @@ class FrontendController extends Controller
 {
 
     public function home(){
-        $posts = Post::orderBy('created_at','DESC')->take(5)->get();
 
+        // Featured Post
+        $posts = Post::with('category', 'user')->orderBy('created_at','DESC')->take(5)->get();
         // Slice
         $firstColPost = $posts->splice(0, 2);
         $middleColPost = $posts->splice(0, 1);
         $lastColPost = $posts->splice(0);
 
-        //return $firstColPost;
+
+
+        // Random Post
+        $randomPosts = Post::with('category', 'user')->inRandomOrder()->limit(4)->get();
+        // Slice
+        $randomfirstColPost = $randomPosts->splice(0, 1);
+        $randommiddleColPost = $randomPosts->splice(0, 2);
+        $randomlastColPost = $randomPosts->splice(0, 1);
+
 
 
         $recentPosts = Post::with('category')->orderBy('created_at','DESC')->paginate(9);
-        return view('website.index', compact(['posts', 'recentPosts', 'firstColPost', 'middleColPost', 'lastColPost']));
+        return view('website.index', compact(['posts', 'recentPosts', 'firstColPost', 'middleColPost', 'lastColPost', 'randomfirstColPost', 'randommiddleColPost', 'randomlastColPost']));
     }
 
     
